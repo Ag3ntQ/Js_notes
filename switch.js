31c84b23f653a4000f60fd74562b970275d47ea9
Ag3ntQ <irfannafi32@gmail.com>
2022-02-07 15:25:25 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,23 @@
+
+var laungage = "js";
+
+switch(laungage){ 
+ case "rb": // if (day=="sunday")
+   document.write("Ruby");
+   break;
+ case "py":  // elif 
+   document.write("Python");
+   break;
+ case "sh":
+   document.write("Bash");
+   break;
+ case "js":
+   document.write("JavaScipt");
+   break;
+ case "go":
+   document.write("Golang");
+   break;
+ default:  // else 
+    document.write("Other laungage");
+    break;
+}
