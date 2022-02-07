f6f1e43ec7e3a193f9cf83d1d0b2f406130c711f
Ag3ntQ <irfannafi32@gmail.com>
2022-02-07 18:20:40 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,28 @@
+/*
+  ---- Loops  ----
+loops are three types
+1) for loop
+2) while loop
+3) do while
+*/
+
+ // for loop
+var x = 1;
+for (x;x<=10;x++){ // from x to 10 with diff 1
+ document.write("for");
+ document.write("</br>");
+}
+  // while loop
+var i = 1;
+while (i<=5){  // while conditon true 
+  document.write("while");
+  document.write("</br>");
+  i++;
+}
+  // do while loop
+var j = 1;
+do{     // do code 
+  document.write("do while");
+  document.write("</br>");
+  j++ 
+}while (j<5)  // while true, exicute do code
