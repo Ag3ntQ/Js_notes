1b87ecd4ae5918c782a345de7a1c17a1ec8be75a
Ag3ntQ <irfannafi32@gmail.com>
2022-02-07 07:57:03 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,21 @@
+var x = 3;
+// Example for if , else
+if (x > 0){
+  document.write("+Ve number ");
+}else{
+  document.write("-Ve number");
+}
+
+// Example for if,else if, else
+// x is a postive integer
+if (x==1){
+  document.write("One");
+}else if(x==2){
+  document.write("Two");
+}else if(x==3){
+  document.write("Three");
+}else if(x==4){
+  document.write("Four");
+}else{
+  document.write("x is greater than 4");
+}
