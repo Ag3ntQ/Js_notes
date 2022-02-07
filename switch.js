19c6845fe278f6492b223b5350320f11ea165071
Ag3ntQ <irfannafi32@gmail.com>
2022-02-07 15:04:57 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,15 @@
+
+var day = "sunday";
+
+switch(day){ 
+ case "sunday": // if (day=="sunday")
+   document.write("Sunday");
+   break;
+ case "Saturday":  // elif 
+   document.write("Saturday");
+   break;
+ default:  // else 
+    document.write("Default Text");
+    break;
+
+}
