87bcc396ebbf9fb51ec6b409d9071958c5f1e4e5
Ag3ntQ <irfannafi32@gmail.com>
2022-02-08 07:27:54 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,22 @@
+/*
+  ----- STRUCTURE -----
+function func_name(){
+  code for exicution;
+}
+
+*/
+
+function test(){
+  alert("Hello world");
+}
+
+function HelloName(name){
+ document.write("Hello"+name+"</br>");
+}
+
+function add(a, b){
+  return a+b;  // return
+}
+
+sum=add(10,15);
+alert(sum)
