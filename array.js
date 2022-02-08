a30184307d00b5744941d6f281ec76501d930b4d
Ag3ntQ <irfannafi32@gmail.com>
2022-02-08 21:39:33 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,38 @@
+
+ // array name = laungage 
+laungage = ["python", "javascript","bash"];
+document.write(laungage);
+document.write("</br>");
+
+// find length of array
+document.write(laungage.length);
+document.write("</br>");
+
+ // access with index number
+document.write(laungage[1]);
+document.write("</br>");
+
+// replace with index number 
+laungage[0] = "golang";
+document.write(laungage);
+document.write("</br>");
+
+// sort
+number = [40,20,60,10];
+number.sort();
+document.write(number);
+document.write("</br>");
+
+           // shift
+ // remove first element
+var laungage = ["python", "javascript","bash","java","cpp","ruby","golang"];
+laungage.shift(); 
+document.write(laungage);
+document.write("</br>");
+
+          // push
+     /* add elements to array */
+laungage = ["python", "javascript","bash"];
+laungage.push("cpp", "java","ruby","golang");
+document.write(laungage);
+document.write("</br>");
