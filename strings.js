4fcd54d882a6df663ce993fe6c5f464b3f6c284b
Ag3ntQ <irfannafi32@gmail.com>
2022-02-09 22:05:17 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,19 @@
+  // var for example
+var text = "Java Script";
+var text1 = "Learn "
+// access with index Number
+console.log(text.charAt(2));
+//    >>>    "v"
+
+// join Strings
+console.log(text1.concat(text));
+//    >>>    "Learn Java Script"
+
+// Change to lower
+console.log(text.toLowerCase())
+//    >>>   "java script"
+
+// Change to Upper case
+console.log(text.toUpperCase());
+//    >>>  "JAVA SCRIPT"
+
